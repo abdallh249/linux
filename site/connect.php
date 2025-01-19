f5b9d957f595733f2a5a56ad81e32bd131788e3f
abdallh249 <abdbadran518@gmail.com>
2025-01-19 17:53:55 +0200

    ih

--- site/connect.php
+++ site/connect.php
@@ -1,14 +1,14 @@
 <?php
-// connect.php
 
-$servername = "db";       // Because our service is named "db" in docker-compose.yml
+
+$servername = "db";       
 $username   = "root";
 $password   = "123";
 $dbname     = "mydb";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check if connection failed
+
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
