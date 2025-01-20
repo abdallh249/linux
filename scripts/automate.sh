634ed5ab98c6bb303bac3f5d8f13c410900a18b2
abdallh249 <abdbadran518@gmail.com>
2025-01-20 11:22:15 +0200

    updet7

--- scripts/automate.sh
+++ scripts/automate.sh
@@ -8,28 +8,19 @@ JENKINS_JOB_URL="https://finer-easily-dassie.ngrok-free.app/job/linux/build" # J
 JENKINS_USER="abed" # Jenkins username
 JENKINS_API_TOKEN="2rqWUDTx5oA7FCoEg344jNO16A4_4k5Ccdu4YgRPmZaUAP6yL" # Jenkins API token
 
-push_to_repository() {
     echo "Pushing changes to the repository..."
     cd "$APP_DIR" || exit
 
     git add .
     git commit -m "Automated update on $(date)"
     git push "$REPO_URL" "$BRANCH"
-}
 
 
-trigger_jenkins_job() {
     echo "Triggering Jenkins job..."
     curl -X POST "$JENKINS_JOB_URL" \
         --user "$JENKINS_USER:$JENKINS_API_TOKEN" \
         -H "Content-Type: application/json"
-}
 
 
-main() {
-    push_to_repository
-    trigger_jenkins_job
-}
 
 
-main
