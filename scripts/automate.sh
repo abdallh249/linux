f8b5ca89d2a3878774d5bf975419474e4df5a9e8
abdallh249 <abdbadran518@gmail.com>
2025-01-20 11:43:58 +0200

    updat8

--- scripts/automate.sh
+++ scripts/automate.sh
@@ -2,11 +2,11 @@
 
 
 APP_DIR="C:\Users\amr20\Desktop"  
-REPO_URL="https://github.com/abdallh249/linux.git" # Remote repository URL
-BRANCH="main" # Target branch
-JENKINS_JOB_URL="https://finer-easily-dassie.ngrok-free.app/job/linux/build" # Jenkins job build URL
-JENKINS_USER="abed" # Jenkins username
-JENKINS_API_TOKEN="2rqWUDTx5oA7FCoEg344jNO16A4_4k5Ccdu4YgRPmZaUAP6yL" # Jenkins API token
+REPO_URL="https://github.com/abdallh249/linux.git" 
+BRANCH="main" 
+JENKINS_JOB_URL="https://finer-easily-dassie.ngrok-free.app/job/linux/build" 
+JENKINS_USER="abed" 
+JENKINS_API_TOKEN="2rqWUDTx5oA7FCoEg344jNO16A4_4k5Ccdu4YgRPmZaUAP6yL" 
 
     echo "Pushing changes to the repository..."
     cd "$APP_DIR" || exit
